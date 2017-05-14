0dfe54028804019fa35de214a548548594842fad
Nils Bjarne Gunnufsen <nils@uxpandable.com>
2017-05-14 11:35:46 +0100

    Added skull with blue stroke

--- Faceit/FaceView.swift
+++ Faceit/FaceView.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class FaceView: UIView {
 
+    var scale : CGFloat = 0.9
 
     override func draw(_ rect: CGRect) {
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        UIColor.blue.set()
+        path.stroke()
     }
 
 
