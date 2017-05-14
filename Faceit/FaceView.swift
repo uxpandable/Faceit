b8d5af271072b5823961fcedfa9ca60e3140de18
Nils Bjarne Gunnufsen <nils@uxpandable.com>
2017-05-14 11:11:56 +0100

    Initial Comitt

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by Nils Bjarne on 14/05/2017.
+//  Copyright © 2017 Nils Bjarne. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
